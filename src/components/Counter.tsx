35ca6d1280bb5c44a9978a65d5dcb0a81f6e3b1b
sooheehong <sooheehong@kakao.com>
2023-08-27 18:17:07 +0900

    3.5 React Hook - useState

---
+++ src/components/Counter.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+type CounterProps = {
+    initalValue: number
+}
+
+const Counter = (props: CounterProps) => {
+    const { initalValue } = props
+
+    const [count, setCount] = useState(initalValue)
+
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count - 1)}>-</button>
+            <button onClick={() => setCount((prevCount) => prevCount + 1)}>-</button>
+        </div>
+    )
+}
