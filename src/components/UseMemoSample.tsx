daf44b727e11e8d7e69340491408b3104b8ae5bb
sooheehong <sooheehong@kakao.com>
2023-08-27 19:44:39 +0900

    3.5.2 useMemo

---
+++ src/components/UseMemoSample.tsx
@@ -0,0 +1,45 @@
+import React, { useCallback, useMemo, useState } from "react"
+
+const UseMemoSample = () => {
+    const [text, setText] = useState('')
+    const [items, setItems] = useState<string[]>([])
+
+    const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setText(e.target.value)
+    }
+
+    const onClickButton = () => {
+        setItems((prevItems) => {
+            return [...prevItems, text]
+        })
+
+        setText('')
+    }
+
+    const numberOfCharacter1 = items.reduce((sub, item) => sub + item.length, 0)
+
+    const numberOfCharacter2 = useMemo(() => {
+        return items.reduce((sub, item) => sub + item.length, 0)
+    }, [items])
+
+    return (
+        <div>
+            <p>UseMemoSample</p>
+            <div>
+                <input value={text} onChange={onChangeInput} />
+                <button onClick={onClickButton}>Add</button>
+            </div>
+            <div>
+                {items.map((item, index) => (
+                    <p key={index}>{item}</p>
+                ))}
+            </div>
+            <div>
+                <p>Total Number of Characters 1 : {numberOfCharacter1}</p>
+                <p>Total Number of Characters 2 : {numberOfCharacter2}</p>
+            </div>
+        </div>
+    )
+}
+
+export default UseMemoSample
